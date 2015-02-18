958eb90ca7323b3360a83992f594ad384f46d9dd
v.bezugly <valnocorner@gmail.com>
2015-02-18 17:08:11 +0300

    added project

---
+++ AGTableViewPaginationExamples/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AGTableViewPaginationExamples
+//
+//  Created by develop on 18/02/15.
+//  Copyright (c) 2015 Allgoritm LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
